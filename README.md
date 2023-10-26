6306b4a41287952082c45ce870a63df5cd74b469
Kalpana Kathait <KALPANAKATHAIT11@GMAIL.COM>
2023-10-26 22:09:50 +0530

    updated README.md file

--- README.md
+++ README.md
@@ -1,23 +1,31 @@
+# Ahead Frontend
+
+Welcome to the Ahead Frontend repository! This project is built with Next.js, gsap, tailwindcss, and the mui library. It implements a redesign concept inspired by the UI design on Dribbble.
 
 ## Getting Started
 
-First, run the development server:
+To get this project up and running on your local machine, follow these steps:
 
-```bash
-npm run dev
-# or
-yarn dev
-```
+1. **Install Dependencies**: Ensure you have Node.js and npm (Node Package Manager) installed. If not, you can download them from the [Node.js Official Website](https://nodejs.org/). Next, navigate to the project directory in your terminal and run:
 
-Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
+    ```bash
+    npm install
+    ```
 
-You can start editing the page by modifying `pages/index.js`. The page auto-updates as you edit the file.
+    This command will install all the necessary project dependencies.
 
-# UI from Dribble: 
+2. **Start the Development Server**: Once the dependencies are installed, you can start the development server by running:
 
-https://dribbble.com/shots/19807069-Ahead-app-redesign-concept
+    ```bash
+    npm run dev
+    ```
 
+    This will launch the application, and you can view it in your web browser at [http://localhost:3000](http://localhost:3000). The page will automatically update as you make changes to the code.
 
-## Link of the Live Website : 
-https://ahead-frontend.vercel.app/
+3. **Explore the UI Design**: The UI design concept for this project was inspired by the Dribbble design concept. You can view the original design on Dribbble: [Ahead App Redesign Concept](https://dribbble.com/shots/19807069-Ahead-app-redesign-concept).
 
+## Live Website
+
+You can access the live version of this project by visiting [Ahead Frontend on Vercel](https://ahead-frontend-mu.vercel.app/).
+
+Please feel free to reach out if you have any questions or feedback. Thank you for your interest in Ahead Frontend. We hope you enjoy exploring the project!
--- app/SecondPage.js
+++ app/SecondPage.js
@@ -1,12 +1,11 @@
-'use client'
+"use client";
 
-import { Paper, Stack, Card } from '@mui/material';
-import { gsap } from 'gsap';
-
-import React, { useEffect, useRef } from 'react';
+import { Paper, Stack, Card } from "@mui/material";
+import { gsap } from "gsap";
+import React, { useEffect, useRef } from "react";
 
 const SecondPage = () => {
-    const cardsRef = useRef();
+  const cardsRef = useRef();
   const headingRef = useRef();
 
   useEffect(() => {
@@ -15,8 +14,8 @@ const SecondPage = () => {
 
     const observerOptions = {
       root: null,
-      rootMargin: '0px',
-      threshold: 0.6, 
+      rootMargin: "0px",
+      threshold: 0.6,
     };
 
     const observer = new IntersectionObserver((entries, observer) => {
@@ -28,15 +27,20 @@ const SecondPage = () => {
           gsap.fromTo(
             cards,
             { opacity: 0, x: 100 },
-            { opacity: 1, x: 0, duration: 2.5, stagger: 0.2, ease: 'power3.out' }
+            {
+              opacity: 1,
+              x: 0,
+              duration: 2.5,
+              stagger: 0.2,
+              ease: "power3.out",
+            }
           );
 
           gsap.fromTo(
             heading,
             { scale: 1.5, opacity: 0 },
-            { scale: 1, opacity: 1, duration: 1.8, ease: 'power3.out' }
-         );
-                
+            { scale: 1, opacity: 1, duration: 1.8, ease: "power3.out" }
+          );
         }
       });
     }, observerOptions);
@@ -44,119 +48,143 @@ const SecondPage = () => {
     if (heading && cards) {
       observer.observe(heading);
     }
-    
-
   }, []);
   return (
-
-
-
-    <Stack 
-    flexDirection='column'
-    display='column'
-    justifyContent='space-evenly'
-    height='100vh'
-    
-    m={10}
-    
+    <Stack
+      flexDirection="column"
+      display="column"
+      justifyContent="space-evenly"
+      height="100vh"
+      m={10}
     >
-        <Stack
-        flexDirection='row'
-        display='column'
-        justifyContent='space-evenly'
+      <Stack
+        flexDirection="row"
+        display="column"
+        justifyContent="space-evenly"
         marginBottom={0}
+      >
+        <span style={{ fontWeight: "bold", fontSize: "40px" }}>
+          EQ Beats IQ{" "}
+        </span>
+        <span style={{ color: "grey", fontSize: "20px" }}>
+          People with high emotional <br></br>intelligence(EQ) live more
+          fulfilled <br></br>lives .they tend to be happier<br></br> have
+          lealthier relationships{" "}
+        </span>
+        <span style={{ color: "grey", fontSize: "20px" }}>
+          People with high emotional<br></br> intelligence(EQ) live more
+          <br></br> fulfilled lives .they tend to be happier<br></br> have
+          lealthier relationships{" "}
+        </span>
+      </Stack>
+
+      <span
+        ref={headingRef}
+        style={{
+          opacity: 0,
+          transform: "scale(1.2)",
+          fontSize: "55px",
+          fontWeight: "bold",
+          marginLeft: "10%",
+        }}
+      >
+        Does this sound familiar...{" "}
+      </span>
+      <Stack
+        flexDirection="row"
+        display="column"
+        ref={cardsRef}
+      >
+        <Card
+          sx={{
+            marginLeft: "3%",
+
+            borderRadius: "2vh",
+            height: "200px",
+            width: "40em",
+            p: "1%",
+          }}
+          style={{ background: "aliceblue" }}
         >
-        <span style={{fontWeight:'bold' , fontSize:'40px'}} >EQ Beats IQ </span>
-        <span  style={{color:'grey', fontSize:'20px' }}>People with high emotional <br></br>intelligence(EQ) live more fulfilled <br></br>lives .they tend to be happier<br></br> have lealthier relationships </span>
-        <span style={{color:'grey', fontSize:'20px' }}>People with high emotional<br></br> intelligence(EQ) live more<br></br> fulfilled lives .they tend to be happier<br></br> have lealthier relationships </span>
-
-        </Stack>
-        
-        <span
-         ref={headingRef} style={{ opacity: 0, transform: 'scale(1.2)' , fontSize:'55px' , fontWeight:'bold' ,marginLeft:'10%'}}>Does this sound familiar... </span>
-        <Stack 
-        flexDirection='row' display='column' ref={cardsRef}
-        // justifyContent='space-evenly'
-        // marginBottom={0}
-        // textAlign={'left'}
-
-        >
-        
-        <Card  
-            
-            sx={{
-                marginLeft:'3%',
-                
-                borderRadius:'2vh',
-                height:'200px',
-                width:'40em',
-                p:'1%'
-
-            }}
-            style={{background: 'aliceblue'}} 
-       >
-
-        <img src='/SecondPage/pic1.png' height={60} ></img>
-        <br></br>
-        <span style={{fontWeight:'bold' , fontSize:'18px'}}>You attend a class reunion </span><br></br>
-        <span style={{fontSize:'14px' , color:'grey'}}>You compare yourself with your peers' achievements, instead of making your self-judgement more independent of others.
-</span>
+          <img src="/SecondPage/pic1.png" height={60}></img>
+          <br></br>
+          <span style={{ fontWeight: "bold", fontSize: "18px" }}>
+            You attend a class reunion{" "}
+          </span>
+          <br></br>
+          <span style={{ fontSize: "14px", color: "grey" }}>
+            You compare yourself with your peers' achievements, instead of
+            making your self-judgement more independent of others.
+          </span>
         </Card>
         <Card
-        sx={{
-                marginLeft:'3%',
-                
-                borderRadius:'2vh',
-                height:'200px',
-                width:'40em',
-                p:'1%'
+          sx={{
+            marginLeft: "3%",
 
-            }}
-            style={{background: 'antiquewhite'}} 
+            borderRadius: "2vh",
+            height: "200px",
+            width: "40em",
+            p: "1%",
+          }}
+          style={{ background: "antiquewhite" }}
         >
-        <img src='/SecondPage/pic2.png' height={60} ></img>
-    <br></br>    <span style={{fontWeight:'bold' , fontSize:'18px'}}>You are at a lively dinner party</span>
-    <br></br>    <span style={{fontSize:'14px' , color:'grey'}}>You play on your phone, instead of confidently approaching strangers and striking up a chat.</span>
+          <img src="/SecondPage/pic2.png" height={60}></img>
+          <br></br>{" "}
+          <span style={{ fontWeight: "bold", fontSize: "18px" }}>
+            You are at a lively dinner party
+          </span>
+          <br></br>{" "}
+          <span style={{ fontSize: "14px", color: "grey" }}>
+            You play on your phone, instead of confidently approaching strangers
+            and striking up a chat.
+          </span>
         </Card>
         <Card
-        sx={{
-                marginLeft:'3%',
-                
-                borderRadius:'2vh',
-                height:'200px',
-                width:'40em',
-                p:'1%'
-
-            }}
-            style={{background: 'lightpink'}} 
+          sx={{
+            marginLeft: "3%",
+            borderRadius: "2vh",
+            height: "200px",
+            width: "40em",
+            p: "1%",
+          }}
+          style={{ background: "lightpink" }}
         >
-        <img src='/SecondPage/pic3.png' height={60}></img>
-    <br></br>    <span style={{fontWeight:'bold' , fontSize:'18px'}}>You hit dead end in a negotiation </span>
-    <br></br>    <span style={{fontSize:'14px' , color:'grey'}}>You get nervous, frazzled, and frustrated, instead of staying optimistic and solution-oriented.</span>
+          <img src="/SecondPage/pic3.png" height={60}></img>
+          <br></br>{" "}
+          <span style={{ fontWeight: "bold", fontSize: "18px" }}>
+            You hit dead end in a negotiation{" "}
+          </span>
+          <br></br>{" "}
+          <span style={{ fontSize: "14px", color: "grey" }}>
+            You get nervous, frazzled, and frustrated, instead of staying
+            optimistic and solution-oriented.
+          </span>
         </Card>
         <Card
-        sx={{
-                marginLeft:'3%',
-                marginRight:'3%',
-                borderRadius:'2vh',
-                height:'200px',
-                width:'40em',
-                p:'1%'
-
-            }}
-            style={{background: 'floralwhite'}} 
+          sx={{
+            marginLeft: "3%",
+            marginRight: "3%",
+            borderRadius: "2vh",
+            height: "200px",
+            width: "40em",
+            p: "1%",
+          }}
+          style={{ background: "floralwhite" }}
         >
-        <img src='/SecondPage/pic4.png' height={60}></img>
-    <br></br>    <span style={{fontWeight:'bold' , fontSize:'18px'}}>You get a promotion at work </span>
-    <br></br>    <span style={{fontSize:'14px' , color:'grey'}}>You question yourself and wonder when they'll realize you're an unqualified imposter, instead of trusting yourself & your abilities.</span>
+          <img src="/SecondPage/pic4.png" height={60}></img>
+          <br></br>{" "}
+          <span style={{ fontWeight: "bold", fontSize: "18px" }}>
+            You get a promotion at work{" "}
+          </span>
+          <br></br>{" "}
+          <span style={{ fontSize: "14px", color: "grey" }}>
+            You question yourself and wonder when they'll realize you're an
+            unqualified imposter, instead of trusting yourself & your abilities.
+          </span>
         </Card>
-        
-
-        </Stack>
-        
-
+      </Stack>
     </Stack>
-  )
-}
+  );
+};
 
-export default SecondPage
+export default SecondPage;
